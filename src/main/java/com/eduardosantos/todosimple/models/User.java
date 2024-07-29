efa7977b6640490b69db0cb2ef3ce646f22352d6
EduardoSSB <eduardossbispo31@gmail.com>
2024-07-29 16:28:31 -0300

    Criado usuário e tarefas

--- src/main/java/com/eduardosantos/todosimple/models/User.java
+++ src/main/java/com/eduardosantos/todosimple/models/User.java
@@ -1,6 +1,7 @@
 package com.eduardosantos.todosimple.models;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -8,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -23,33 +25,32 @@ public class User {
     public interface CreateUser {} //Interface para validação de criação de usuário
     public interface UpdateUser {} //Interface para validação de atualização de usuário
 
-    public static final String TABLE_NAME = "user";
+    public static final String TABLE_NAME = "users";
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE) //Gera valores sequenciais no banco (identity do postgres)
     @Column(name = "id", unique = true, nullable = false) //Verificação de coluna no banco, para criar a tabela
     private Long id;
-    
 
     //(groups = CreateUser.class) é uma forma de dizer que a validação só deve ser feita quando o objeto for criado
     //{CreateUser.class, UpdateUser.class} é uma forma de dizer que a validação deve ser feita tanto na criação quanto na atualização
 
-    @Column(name = "username", unique = true, nullable = false, length = 50)
+    @Column(name = "username", unique = true, nullable = false, length = 100)
     @NotNull (groups = CreateUser.class) //Verifica se o valor do campo não é null, antes de mandar pro banco
     @NotEmpty (groups = CreateUser.class)
     @Size(min = 3, max = 50) //Verifica se o tamanho da string está entre 3 e 50
     private String username;
 
     @JsonProperty(access = Access.WRITE_ONLY) //Não permite que o campo seja lido, apenas escrito
-    @Column(name = "password", nullable = false)
+    @Column(name = "password", nullable = false, length = 20)
     @NotBlank (groups = {CreateUser.class, UpdateUser.class}) //O equivalente ao @NotNull e @NotEmpty ao mesmo tempo
     @Size(min = 6, max = 20) //Verifica se o tamanho da string está entre 6 e 20
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
+    //mappedBy serve para dizer que o mapeamento foi feito na outra classe, uso o nome da variável que está na outra classe
+    @OneToMany(mappedBy = "user") //Fazendo o contrário do que foi feito no Task
+    private List<Task> tasks = new ArrayList<Task>();
 
-    public User() {
-    }
 
     public User(Long id, String username, String password) {
         this.id = id;
@@ -79,6 +80,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     /*Verifica se o objeto é nulo, se não é uma instância de User, 
